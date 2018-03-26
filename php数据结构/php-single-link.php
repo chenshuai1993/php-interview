d11447bcf0f7be7b0dfc72cef6fe39f3f7eb94ad
shuaishuai.chen <852438363@qq.com>
2018-03-26 10:19:51 +0800

    单链表优化
    单链表优化

--- php数据结构/php-single-link.php
+++ php数据结构/php-single-link.php
@@ -25,7 +25,6 @@ class node
 
 
 
-
 //单链表对象
 class singleLink
 {
@@ -34,22 +33,53 @@ class singleLink
 
     public $size;
 
-
-    public function add(node $node)
+    /**
+     * @param node $node
+     * @param int $preNodeId
+     * @return $this 链式增加
+     */
+    public function add(node $node, int $preNodeId = 0)
     {
-        //循环
-        if($this->size == 0){
+        ##循环
+
+        #空链表 直接头插
+        if($this->size == 0) {
+
             $this->head = $node;
-        }else{
-            //单链表 头插
-            $node->next =  $this->head; //当前node的next 指向 $this->head->id
+
+        } elseif ($preNodeId == 0) {
+
+            //如果不是空链表 并且没有指定前置节点id,默认的话,链表头插
+            $node->next =  $this->head;  //当前node的next 指向 $this->head
             $this->head =  $node;        //当前node 设置为单链表头
+
+        } elseif($preNodeId != 0) {
+
+            //不是空链表,并且设置了前置节点id
+            $curr = $this->head;
+
+            while ($curr->next != null)
+            {
+                if($curr->next->id == $preNodeId){  //当前节点的下一个节点id == 要查找的前置节点
+                    $node->next = $curr->next;
+                    $curr->next = $node;
+                    break;
+                }
+                $curr = $curr->next; //没有匹配到前置节点， 那么就下一个,which在进行下一次判断
+            }
+
         }
 
         $this->size++;
 
+        return $this;
     }
 
+    /**
+     * @删除单链表元素
+     * @param $index
+     * @return bool
+     */
     public function del($index)
     {
         //循环
@@ -74,28 +104,40 @@ class singleLink
     }
 
 
-    //头插
+    /**
+     * @获取单链表的元素
+     * @param $index
+     * @return $node
+     */
     public function get($index)
     {
         $curr = $this->head;
 
+        $node = [];
         while ($curr->id != null){
             if($curr->id == $index){
                 $node = $curr;
-                return $node;
                 break;
             }
             $curr = $curr->next;
         }
 
+        return $node;
 
     }
 
 
+    /**
+     * @编辑单链表
+     * @param $index
+     * @param $val
+     * @return bool
+     */
     public function edit($index,$val)
     {
         $curr = $this->head;
         $flag = false;
+        
         while ($curr->id != null){
             if($curr->id == $index){
                 $curr->data = $val;
@@ -125,10 +167,7 @@ $list = new singleLink();
 
 
 #增加
-$list->add($node1);
-$list->add($node2);
-$list->add($node3);
-$list->add($node4);
+$list->add($node1)->add($node2)->add($node3)->add($node4);
 
 
 #删除
@@ -139,7 +178,7 @@ $list->add($node4);
 #$list->edit(3,'3333');
 
 #查
-#print_r($list->get(4));
+#print_r($list->get(3));
 
 #print_r($node1);
 #print_r($node2);
