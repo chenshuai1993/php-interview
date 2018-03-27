c6923a908e26a1de252beb2ef875d3863e30d1d6
shuaishuai.chen <852438363@qq.com>
2018-03-27 10:17:34 +0800

    模拟调用过程
    模拟调用过程

--- php数据结构/php-quick-sort.php
+++ php数据结构/php-quick-sort.php
@@ -12,7 +12,8 @@
 //      递归比较,知道ok
 
 
-$array = [10,3,5,14,38,4,67,9,1,88];
+$array = [10,3,5,14,38];
+
 
 
 $list = quick_sort($array);
@@ -39,11 +40,79 @@ function quick_sort($array)
         $left = quick_sort($left);
         $right = quick_sort($right);
 
+        # print_r(array_merge($left,array($compare),$right));
+        #echo PHP_EOL;
         return array_merge($left,array($compare),$right);
     }else{
+        #print_r($array);
+        #echo PHP_EOL;
         return $array;
     }
 
 }
 
+
+//打印过程如下,便于理解
+
+
+/*
+ *
+        一次调用               10 3 5 14 38
+                               mid  10
+          2次        left [3,5]                    [14,38]  right
+                        mid 3                   mid 14
+            3次  left [] , 4次  [5] right        left[],   [38] right
+                return[],        reutrn [5]      return [],  return [38]
+                   merge([],3[5])                mergr([],[14],[38])
+
+                    mergr([3,5],[10],[14,18])
+
+
+Array
+(
+)
+
+Array
+(
+    [0] => 5
+)
+
+Array
+(
+    [0] => 3
+    [1] => 5
+)
+
+Array
+(
+)
+
+Array
+(
+    [0] => 38
+)
+
+Array
+(
+    [0] => 14
+    [1] => 38
+)
+
+Array
+(
+    [0] => 3
+    [1] => 5
+    [2] => 10
+    [3] => 14
+    [4] => 38
+)
+
+Array
+(
+    [0] => 3
+    [1] => 5
+    [2] => 10
+    [3] => 14
+    [4] => 38
+)*/
 
